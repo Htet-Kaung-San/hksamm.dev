e32ee6041092af352a9ce7e1565849133ece06d7
hksamm <htetsan206@gmail.com>
2026-02-18 00:15:10 +0900

    mouse click effect removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Testimonials, Sidebar, Services, Resume, Pricing, Portfolio, Home, Contact, Blog, About, TechStack } from './components';
 import './App.css';
 import ThemeToggle from './components/themeToggle/ThemeToggle';
-import ClickSpark from './animations/ClickSpark';
 import { useTheme } from './context/theme';
 import UserInfo from './components/UserInfo';
 import { Analytics } from '@vercel/analytics/react';
@@ -31,20 +30,18 @@ function App() {
 			<ThemeToggle />
 			<Sidebar />
 			<main className="main">
-				<ClickSpark sparkColor={isDarkMode ? '#FF4757' : '#1A1A2E'} sparkSize={20} sparkRadius={70} sparkCount={10} duration={800} extraScale={1.2}>
-					<>
-						<Home />
-						<About />
-						<Services />
-						<TechStack />
-						<Resume />
-						<Portfolio />
-						{/* <Pricing /> */}
-						<Blog />
-						<Testimonials />
-						<Contact />
-					</>
-				</ClickSpark>
+				<>
+					<Home />
+					<About />
+					<Services />
+					<TechStack />
+					<Resume />
+					<Portfolio />
+					{/* <Pricing /> */}
+					<Blog />
+					<Testimonials />
+					<Contact />
+				</>
 			</main>
 			<Analytics />
 			<SpeedInsights />
