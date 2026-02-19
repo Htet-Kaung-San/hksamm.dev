42aeb39cad82edaba898bf588a866ae6a578b8a6
hksamm <htetsan206@gmail.com>
2026-02-20 06:21:07 +0900

    hero section updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import Me from '../../assets/myface.png';
+import Me from '../../assets/animation-gif.gif';
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 import Shapes from './Shapes';
@@ -9,20 +9,24 @@ import TextType from '../../animations/textType/TextType';
 const Home = () => {
 	return (
 		<section className="home container" id="home">
-			<div className="intro">
-				<div className="home__img-wrapper">
-					<img src={Me} alt="" className="home__img" />
-					<div className="home__img-decoration"></div>
+			<div className="home__content">
+				<div className="intro">
+					<h1 className="home__name">Htet Kaung San</h1>
+					<span className="home__education">Full-Stack Engineer & Designer</span>
+					<div className="memphis-bg-texttype">
+						<TextType text={['Crafting Code & Shaping Solution', 'Code Artist + Problem Solver', 'Simplifying Complexity + Enhancing Experience']} typingSpeed={75} pauseDuration={2000} showCursor={true} cursorCharacter="●" />
+					</div>
+					<HeaderSocials />
+					<a href="#contact" className="btn">
+						Let's Talk
+					</a>
 				</div>
-				<h1 className="home__name">Htet Kaung San</h1>
-				<span className="home__education">Full-Stack Engineer & Designer</span>
-				<div className="memphis-bg-texttype">
-					<TextType text={['Crafting Code & Shaping Solution', 'Code Artist + Problem Solver', 'Simplifying Complexity + Enhancing Experience']} typingSpeed={75} pauseDuration={2000} showCursor={true} cursorCharacter="●" />
+				<div className="home__media">
+					<div className="home__img-wrapper">
+						<img src={Me} alt="" className="home__img" />
+						<div className="home__img-decoration"></div>
+					</div>
 				</div>
-				<HeaderSocials />
-				<a href="#contact" className="btn">
-					Let's Talk
-				</a>
 			</div>
 			<ScrollDown />
 			<Shapes />
